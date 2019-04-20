425711b4b38bf97e16876e1619fed7f5b7664753
Edvin Hultberg <hultberg@mittnett.net>
2019-04-20 14:06:58 +0200

    Improvements and new version

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::io;
 use std::ops;
 use std::collections::HashMap;
 use rand::Rng;
-use std::char;
 
 struct TerminalInfo {
     height: u16,
@@ -24,7 +23,15 @@ impl CardRank {
             CardRank::Diamond => String::from("♦"),
             CardRank::Spade => String::from("♠"),
             CardRank::Clover => String::from("♣"),
-            _ => String::from("LOL"),
+        }
+    }
+
+    pub fn get_rank_id(&self) -> u8 {
+        match self {
+            CardRank::Heart => 1,
+            CardRank::Diamond => 2,
+            CardRank::Spade => 3,
+            CardRank::Clover => 4,
         }
     }
 }
@@ -56,13 +63,14 @@ impl Player {
     }
 
     fn get_card_labels(&self) -> Vec<String> {
-        let mut labels = vec![];
-
-        for &card in self.cards.iter() {
-            labels.push(card.get_label());
-        }
-
-        labels
+        let mut all_cards: Vec<_> = self.cards.to_vec();
+        all_cards.sort_by(|a, b| {
+            let a_rank_num = a.rank.get_rank_id();
+            let b_rank_num = b.rank.get_rank_id();
+            return a_rank_num.cmp(&b_rank_num).then_with(|| a.number.cmp(&b.number));
+        });
+        let all_cards_iterator = all_cards.into_iter();
+        all_cards_iterator.map(|card| card.get_label()).collect()
     }
 }
 
@@ -89,6 +97,30 @@ impl Card {
         let mut label = self.rank.get_label().to_string();
         label.push_str(&num_key);
         label
+    }
+}
+
+struct GameLog {
+    status_lines: Vec<String>,
+    turn: usize,
+}
+
+impl GameLog {
+    fn new() -> GameLog {
+        GameLog {
+            turn: 1,
+            status_lines: vec![],
+        }
+    }
+
+    fn clear(&mut self) {
+        self.status_lines.clear();
+    }
+
+    fn add_status_line(&mut self, message: String) {
+        let mut message_formatted = format!("T{} - ", self.turn);
+        message_formatted.push_str(&message);
+        self.status_lines.push(message_formatted);
     }
 }
 
@@ -129,45 +161,34 @@ fn main() {
     give_random_card(&mut deck, &mut player, 7);
     give_random_card(&mut deck, &mut opponent, 7);
 
-    let mut current_status_lines: Vec<String> = vec![];
+    let mut game_log = GameLog::new();
 
     loop {
         // create books.
-        check_cards_for_books(&mut current_status_lines, &mut player);
-        check_cards_for_books(&mut current_status_lines, &mut opponent);
+        check_cards_for_books(&mut game_log, &mut player);
+        check_cards_for_books(&mut game_log, &mut opponent);
 
-        println!("{}{}Go Fish v0.2.0",
+        println!("{}{}Go Fish v0.3.0",
             termion::clear::All,
             termion::cursor::Goto(1, 1));
 
-        let game_standings: Vec<(&String, usize)> = {
-            let all_players: Vec<&Player> = vec![&player, &opponent];
-
-            let mut book_counts: HashMap<&String, usize> = HashMap::new();
-            for &player in all_players.iter() {
-                book_counts.insert(&player.name, player.books.len());
-            }
-            let mut book_counts: Vec<_> = book_counts.into_iter().collect();
-            book_counts.sort_by(|a, b| a.1.cmp(&b.1).reverse());
-            book_counts
-        };
+        let mut game_standings: Vec<&Player> = vec![&player, &opponent];
+        game_standings.sort_by(|a, b| {
+            return a.books.len().cmp(&b.books.len()).reverse().then_with(|| a.name.cmp(&b.name));
+        });
 
         if let Some(winner) = determine_winner(&game_standings) {
             gameover(&winner);
             break;
         }
 
-        let player_books_string: String = {
-            let mut player_books_strings: Vec<String> = vec![];
-            for (this_player, num_books) in game_standings.into_iter() {
-                player_books_strings.push(format!("{}({})", this_player, num_books));
-            }
+        println!("{}{} cards in deck", termion::cursor::Goto(1, 2), deck.len());
 
-            let mut player_books_string = String::from("Books: ");
-            player_books_string.push_str(&player_books_strings.join(", "));
-            player_books_string
+        let player_books_string: String = {
+            let player_books_strings: Vec<String> = game_standings.into_iter().map(|this_player| format!("{} ({})", this_player.name, this_player.books.len())).collect();
+            format!("Standings: {}", player_books_strings.join(", "))
         };
-        println!("{}{}", termion::cursor::Goto(1, 2), player_books_string);
+        println!("{}{}", termion::cursor::Goto(1, 3), player_books_string);
 
         let player_cards_string = player.get_card_labels().join(", ");
         println!("{}{}",
@@ -175,18 +196,20 @@ fn main() {
             player_cards_string);
 
         // print status lines
-        for (pos, line) in current_status_lines.iter().enumerate() {
+        for (pos, line) in game_log.status_lines.iter().enumerate() {
             println!("{}{}",
                 termion::cursor::Goto(1, terminal_info.height - 2 - (pos as u16)),
                 line);
         }
 
-        // clear the status lines
-        current_status_lines.clear();
+        if !player.is_cpu {
+            // clear the last game log
+            game_log.clear();
+        }
 
-        let (next_player, next_opponent) = turn(
+        let (next_player, next_opponent, is_next_turn) = turn(
             &terminal_info,
-            &mut current_status_lines,
+            &mut game_log,
             &mut deck,
             player,
             opponent
@@ -194,39 +217,44 @@ fn main() {
 
         player = next_player;
         opponent = next_opponent;
+
+        if is_next_turn {
+            game_log.add_status_line(String::from("End of turn"));
+            game_log.turn += 1;
+        }
     }
 }
 
 // FIXME: Pass Player structs instead of String
-fn determine_winner<'a>(standings: &'a Vec<(&String, usize)>) -> Option<&'a String> {
+fn determine_winner<'a>(standings: &'a Vec<&Player>) -> Option<&'a Player> {
     let mut total_book_count = 0;
 
-    for (_player_name, book_count) in standings.iter() {
-        total_book_count = total_book_count + *book_count;
+    for &player in standings.iter() {
+        total_book_count = total_book_count + player.books.len();
     }
 
     if total_book_count >= 13 {
-        return Some(&standings[0].0);
+        return Some(&standings[0]);
     }
 
     return None;
 }
 
-fn gameover(winner: &String) {
-    println!("{}{} has won", termion::clear::All, winner);
+fn gameover(winner: &Player) {
+    println!("{}{} has won with {} books", termion::clear::All, winner.name, winner.books.len());
 }
 
 fn turn(
     terminal_info: &TerminalInfo,
-    current_status_lines: &mut Vec<String>,
+    game_log: &mut GameLog,
     mut deck: &mut Vec<Card>,
     mut current_player: Player,
     mut current_opponent: Player,
-) -> (Player, Player) {
+) -> (Player, Player, bool) {
     let card_face_value: u8 = {
         if current_player.is_cpu {
             let card_value = get_cpu_requesting_card_value(&current_player);
-            current_status_lines.push(format!("Computer requested {}", card_value));
+            game_log.add_status_line(format!("Computer requested {}", card_value));
             card_value
         } else {
             get_player_requesting_card_value(&terminal_info)
@@ -234,8 +262,8 @@ fn turn(
     };
 
     if card_face_value < 2 || card_face_value > 14 {
-        current_status_lines.push(String::from("Invalid card face value"));
-        return (current_player, current_opponent);
+        game_log.add_status_line(String::from("Invalid card face value"));
+        return (current_player, current_opponent, false);
     }
 
     let mut has_card: bool = false;
@@ -247,8 +275,8 @@ fn turn(
     }
 
     if !has_card && !current_player.is_cpu {
-        current_status_lines.push(String::from("You dont have that card"));
-        return (current_player, current_opponent);
+        game_log.add_status_line(String::from("You dont have that card"));
+        return (current_player, current_opponent, false);
     }
 
     let mut found_cards: Vec<Card> = vec![];
@@ -262,30 +290,40 @@ fn turn(
     let found_cards_length = found_cards.len();
 
     if found_cards_length > 0 {
-        current_status_lines.push(String::from("Opponent had card"));
-
         for card in found_cards.iter() {
+            game_log.add_status_line(String::from(format!("{} -{}-> {}",
+                current_opponent.name,
+                card.get_label(),
+                current_player.name)));
             current_player.cards.push(*card);
         }
 
         found_cards.clear();
 
         current_opponent.cards.retain(|&card| card.number != card_face_value);
-        return (current_player, current_opponent);
+        return (current_player, current_opponent, false);
     }
 
     if deck.len() <= 0 {
-        return (current_opponent, current_player);
+        return (current_opponent, current_player, true);
     }
 
     give_random_card(&mut deck, &mut current_player, 1);
-    let first_card = current_player.cards.last().unwrap();
-    current_status_lines.push(String::from(format!("Go Fish, you got {} from the deck", first_card.get_label())));
 
-    return (current_opponent, current_player);
+    let last_card = current_player.cards.last().unwrap();
+    let mut last_card_label = String::new();
+    if !current_player.is_cpu {
+        last_card_label = last_card.get_label();
+    }
+
+    game_log.add_status_line(String::from(format!("Go Fish, deck -{}-> {}",
+        last_card_label,
+        current_player.name)));
+
+    return (current_opponent, current_player, true);
 }
 
-fn check_cards_for_books(current_status_cards: &mut Vec<String>, current_player: &mut Player) {
+fn check_cards_for_books(game_log: &mut GameLog, current_player: &mut Player) {
     let mut cards_count: HashMap<u8, u8> = HashMap::new();
 
     for &card in current_player.cards.iter() {
@@ -298,14 +336,7 @@ fn check_cards_for_books(current_status_cards: &mut Vec<String>, current_player:
                     number: card.number,
                 });
 
-                let has_books_status = {
-                    let mut str = String::new();
-                    str.push_str(&current_player.name);
-                    str.push_str(" now has ");
-                    str.push(char::from_digit(current_player.books.len() as u32, 10).unwrap());
-                    str.to_string()
-                };
-                current_status_cards.push(has_books_status);
+                game_log.add_status_line(String::from(format!("{} collected a book of {}", current_player.name, card.number)));
             }
         };
     }
